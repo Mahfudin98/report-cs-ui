ad3d4d21c90d72b8196d3780bbc3ca61f324a997
Mahfudin98 <mahfudinnawawi98@gmail.com>
2023-08-09 09:20:44 +0700

    add filter member by cs id

--- src/pages/CS/Member/MemberData.vue
+++ src/pages/CS/Member/MemberData.vue
@@ -1,7 +1,11 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script setup>
 import { mapActions, mapState } from "vuex";
-import { PencilSquareIcon, EyeIcon } from "@heroicons/vue/24/outline";
+import {
+  PencilSquareIcon,
+  EyeIcon,
+  MagnifyingGlassIcon,
+} from "@heroicons/vue/24/outline";
 import LoadingScreenVue from "@/components/Widget/LoadingScreen.vue";
 </script>
 <template>
@@ -25,24 +29,29 @@ import LoadingScreenVue from "@/components/Widget/LoadingScreen.vue";
         </router-link>
         <form class="flex items-center gap-2">
           <div>
+            <select
+              v-model="data.filterCS"
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            >
+              <option value="all">All</option>
+              <option
+                v-for="(item, index) in usersCS"
+                :key="index"
+                :value="item.user_id"
+              >
+                {{ item.nama_depan }}
+              </option>
+            </select>
+          </div>
+          <div>
             <label for="simple-search" class="sr-only">Search</label>
             <div class="relative w-full">
               <div
                 class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"
               >
-                <svg
-                  aria-hidden="true"
+                <MagnifyingGlassIcon
                   class="w-5 h-5 text-gray-500 dark:text-gray-400"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
+                />
               </div>
               <input
                 type="text"
@@ -60,8 +69,8 @@ import LoadingScreenVue from "@/components/Widget/LoadingScreen.vue";
               class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             >
               <option value="all">All</option>
-              <option value="agen">Agen</option>
-              <option value="reseller">Reseller</option>
+              <option value="1">Agen</option>
+              <option value="0">Reseller</option>
             </select>
           </div>
         </form>
@@ -194,13 +203,17 @@ import LoadingScreenVue from "@/components/Widget/LoadingScreen.vue";
 export default {
   created() {
     this.getIndexMember();
-    this.getUsersLogin();
+    this.getUsersLogin().then(() => {
+      this.data.filterCS = this.userLogin.id;
+    });
+    this.getUsersCS();
   },
   data() {
     return {
       data: {
         search: "",
         filterKey: "all",
+        filterCS: "all",
       },
       fakeImage:
         "https://ui-avatars.com/api/?name=LS+Skincare&background=random&size=90&bold=true",
@@ -208,23 +221,40 @@ export default {
   },
   computed: {
     ...mapState("member", { members: (state) => state.member_index }),
-    ...mapState("user", { userLogin: (state) => state.userLogin }),
+    ...mapState("user", {
+      userLogin: (state) => state.userLogin,
+      usersCS: (state) => state.usersCS,
+    }),
     dataFilter() {
-      return this[this.data.filterKey];
+      let filter =
+        this.data.filterCS == "all"
+          ? this[this.data.filterCS]
+          : this.filterData;
+      return filter;
     },
     all() {
-      return this.members.data;
+      let data = this.members.data;
+      let filter =
+        this.data.filterKey == "all"
+          ? data
+          : data.filter((i) => {
+              return i.member_type == this.data.filterKey;
+            });
+      return filter;
     },
-    agen() {
-      return this.members.data.filter((item) => {
-        return item.member_type == 1;
+    filterData() {
+      let data = this.members.data.filter((item) => {
+        return item.cs_id == this.data.filterCS;
       });
+      let filter =
+        this.data.filterKey == "all"
+          ? data
+          : data.filter((i) => {
+              return i.member_type == this.data.filterKey;
+            });
+      return filter;
     },
-    reseller() {
-      return this.members.data.filter((item) => {
-        return item.member_type == 0;
-      });
-    },
+
     search() {
       return this.data.search;
     },
@@ -236,7 +266,7 @@ export default {
   },
   methods: {
     ...mapActions("member", ["getIndexMember", "getSelectMember"]),
-    ...mapActions("user", ["getUsersLogin"]),
+    ...mapActions("user", ["getUsersLogin", "getUsersCS"]),
   },
 };
 </script>
